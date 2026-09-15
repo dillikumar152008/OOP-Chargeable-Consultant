298088253959b7749b4c899966650a886ebcec27
dillikumar152008 <dillikumar152008@gamil.com>
2026-09-15 22:17:36 +0530

    Update consultant fee calculation

--- Consultant.java
+++ Consultant.java
@@ -4,12 +4,24 @@ interface Chargeable {
 
 class Consultant implements Chargeable {
 
+    int hours;
+    double feePerHour;
+
+    Consultant(int hours, double feePerHour) {
+        this.hours = hours;
+        this.feePerHour = feePerHour;
+    }
+
     public void chargeFee() {
-        System.out.println("Consultant charges a consultation fee.");
+        double totalFee = hours * feePerHour;
+
+        System.out.println("Consultation Hours: " + hours);
+        System.out.println("Fee Per Hour: ₹" + feePerHour);
+        System.out.println("Total Consultation Fee: ₹" + totalFee);
     }
 
     public static void main(String[] args) {
-        Consultant c = new Consultant();
+        Consultant c = new Consultant(5, 1000);
         c.chargeFee();
     }
 }
